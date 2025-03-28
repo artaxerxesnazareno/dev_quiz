b0ede945dd0fc3ce760eae713a9969d2cd85fb11
= <artaxerxesnazare@hotmail.com>
2025-03-28 04:31:29 +0100

    ✨ feat(appbar): adiciona widget de cartão de pontuação
    Implementa o ScoreCardWidget na AppBarWidget, incluindo:
    - Novo layout com gradiente e container empilhados
    - Integração do ChartWidget para exibir progresso
    - Ajustes de estilo e posicionamento dos elementos

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:dev_quiz/core/app_gradients.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/cupertino.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -8,38 +9,45 @@ class AppBarWidget extends PreferredSize {
         preferredSize: Size.fromHeight(250),
         child: Container(
           height: 250,
-          decoration: BoxDecoration(gradient: AppGradients.linear),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text.rich(
-                  TextSpan(
-                    text: 'Olá, ',
-                    style: AppTextStyles.title,
-                    children: [
+          child: Stack(
+            children: [
+              Container(
+                height: 161,
+                decoration: BoxDecoration(gradient: AppGradients.linear),
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text.rich(
                       TextSpan(
-                        text: 'Artaxerxes',
-                        style: AppTextStyles.titleBold,
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 58,
-                  height: 58,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        'https://avatars.githubusercontent.com/u/105333397?v=4',
+                        text: 'Olá, ',
+                        style: AppTextStyles.title,
+                        children: [
+                          TextSpan(
+                            text: 'Artaxerxes',
+                            style: AppTextStyles.titleBold,
+                          ),
+                        ],
                       ),
                     ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                    Container(
+                      width: 58,
+                      height: 58,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            'https://avatars.githubusercontent.com/u/105333397?v=4',
+                          ),
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Align(alignment: Alignment(0, 1), child: ScoreCardWidget()),
+            ],
           ),
         ),
       );
